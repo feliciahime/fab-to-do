e06bea6180ea922e4d0a84267069bebd14bfe35d
Felicia Becerra <16847624+feliciahime@users.noreply.github.com>
2021-03-31 18:35:46 -0700

    Adding functions, hiding checkbox if no data in list

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+console.log("Booting up what to do...");
 
 function App() {
+  const [toDoItem, setToDoItem] = useState([]);
+  const [isDeleted, setDeleted] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+
+  function onAddItem (ev) {
+    console.log('This button works');
+
+  }
+
+  function onToDoItemChange (ev) {
+    let value = ev.target.value;
+    setToDoItem(value);
+  }
+
+function onShowList (ev) {
+
+}
+
+
+  // function onCheckOff (ev) {
+  //   let value = ev.target.value;
+  //   if (setIsDone === false) (
+  //     setIsDone(true);
+  //   ) else: (
+  //     setIsDone(false))
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-      <style>
-      @import url('https://fonts.googleapis.com/css2?family=Meddon&display=swap');
-      </style>
-       
-       <div>
-        <h1 className="title">My To-Do list</h1>
-       </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+        <header className="App-header">
+          <style>@import url('https://fonts.googleapis.com/css2?family=Meddon&display=swap');
+          </style>
+          <div className="title">
+            <h1>My To-Do list</h1>
+          </div>
+        </header>
+        
+        <body>
+        <div className="Container">
+          <p><textarea id="newitem"
+          placeholder="Add an item to the list"
+          value={toDoItem}
+          onChange={onToDoItemChange}
+          /></p>
+          <p><button onClick={onAddItem}> Add me! </button></p>
 
-      <footer>
-      <h5>(C) Fabulous Productions 2021</h5>
-      </footer>
+          <div className="Checkbox">
+            <p>{ 
+              toDoItem.length === 0 ? 
+              `<input type="checkbox" defaultChecked={false} /> {toDoItem}` :  ''      }</p>
+          </div>
+
+        </div>
+        </body>
+      <footer><h5>(C) Fabulous Productions 2021</h5></footer>
+    
     </div>
   );
 }
