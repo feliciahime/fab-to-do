d75ddca7507a9323e06366105d66390741e0dcf4
Felicia Becerra <16847624+feliciahime@users.noreply.github.com>
2021-04-05 11:09:06 -0700

    adding items and checklist working

--- README.md
+++ README.md
@@ -13,3 +13,54 @@ This project uses React to create a To-do list.
 // dark green 41585A
 // deep green 1F352F
 // beige E8D5B3
+
+
+
+// 
+
+<div className="Container">
+            <div className="Form">
+            <input
+          placeholder="Add a thing to do"
+          value={toDoItem}
+          onChange={onToDoItemChange}
+        />
+          
+
+
+function onShowList (ev) {
+let total = toDoItem.length;
+  if (total > 0) {
+    return (
+      <ul className="item-list">
+          <li />{toDoItem.item} <input type="checkbox" defaultChecked={false} />
+      </ul>
+    );
+  }
+}
+
+  // function onCheckOff (ev) {
+  //   let value = ev.target.value;
+  //   if (setIsDone === false) (
+  //     setIsDone(true);
+  //   ) else: (
+  //     setIsDone(false))
+  // }
+
+
+  function onToDoItemChange (ev) {
+    let value = ev.target.value;
+    setToDoItem(value);
+    console.log('your new item is: ', value);
+    console.log(toDoItem);
+  }
+
+
+
+    const listItems = toDoItem.map((todo)) =>
+    <li>{todo}</li>
+
+
+                  <ul className="item-list">
+              <li />{toDoItem} <input type="checkbox" defaultChecked={false} />
+              </ul>
--- src/App.js
+++ src/App.js
@@ -4,61 +4,75 @@ import './App.css';
 console.log("Booting up what to do...");
 
 function App() {
-  const [toDoItem, setToDoItem] = useState([]);
+  const [toDoItem, setToDoItem] = React.useState("");
+  const [itemDescription, setItemDescription] = React.useState("");
+  const [list, setList] =  React.useState([]);
   const [isDeleted, setDeleted] = useState(false);
-  const [isDone, setIsDone] = useState(false);
+
 
   function onAddItem (ev) {
     console.log('This button works');
-
-  }
+    ev.preventDefault();
+    setList([...list, { toDoItem: toDoItem, itemDescription: itemDescription}
+      ]);
+    }
+      
 
   function onToDoItemChange (ev) {
-    let value = ev.target.value;
-    setToDoItem(value);
-  }
+      let value = ev.target.value;
+      let name = ev.target.name;
 
-function onShowList (ev) {
+      if (name === 'toDoItem') { setToDoItem(value)}
+      if (name === 'itemDescription') {setItemDescription(value)}
+      if (name === 'isDeleted') {setDeleted(value)}
 
-}
-
-
-  // function onCheckOff (ev) {
-  //   let value = ev.target.value;
-  //   if (setIsDone === false) (
-  //     setIsDone(true);
-  //   ) else: (
-  //     setIsDone(false))
-  // }
+      setToDoItem(value);
+      console.log('updated item: ', value);
+    }
 
 
   return (
     <div className="App">
         <header className="App-header">
-          <style>@import url('https://fonts.googleapis.com/css2?family=Meddon&display=swap');
-          </style>
           <div className="title">
             <h1>My To-Do list</h1>
           </div>
         </header>
         
-        <body>
+
+
         <div className="Container">
-          <p><textarea id="newitem"
-          placeholder="Add an item to the list"
-          value={toDoItem}
-          onChange={onToDoItemChange}
-          /></p>
-          <p><button onClick={onAddItem}> Add me! </button></p>
-
-          <div className="Checkbox">
-            <p>{ 
-              toDoItem.length === 0 ? 
-              `<input type="checkbox" defaultChecked={false} /> {toDoItem}` :  ''      }</p>
+          <div className="item-list">
+            {list.map(item => {
+              return (
+                <div><b>{item.toDoItem}  </b> {item.itemDescription} <input type="checkbox" defaultChecked={false} />
+                </div>
+                );
+            })}
           </div>
-
         </div>
-        </body>
+
+        <div className="item-form">
+          <form onSubmit={onAddItem}>
+            <label>Item:
+              <input
+              placeholder="Add a thing to do "
+              value={toDoItem}
+              onChange={e => setToDoItem(e.target.value)}
+              />
+            </label>
+            <label><p> Details: 
+              <input
+              placeholder=" "
+              value={itemDescription}
+              onChange={e => setItemDescription(e.target.value)}
+              /></p>
+              <button>Submit</button>
+            </label>
+          </form>
+        </div>
+
+
       <footer><h5>(C) Fabulous Productions 2021</h5></footer>
     
     </div>
